7ef0d8d82a711f59255f9ab0c09719a7a0fbc6f7
Jonathan Rey <jonatrey@reziew.com>
2014-01-23 17:30:39 +0100

    Testing Thymeleaf - passing params from controller to view

--- src/main/java/com/searchbox/web/HomeController.java
+++ src/main/java/com/searchbox/web/HomeController.java
@@ -1,5 +1,7 @@
 package com.searchbox.web;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,10 +11,15 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 @RequestMapping("/")
 public class HomeController {
+	
+	private static Logger logger = LoggerFactory.getLogger(HomeController.class);
+	
 
 	@RequestMapping
 	public ModelAndView index(@RequestParam(defaultValue="unknown") String h) {
 		ModelAndView model = new ModelAndView("index");
+		
+		logger.debug("Here is our param2 "+h);
 		model.addObject("h", h);
 		return model;
 	}
