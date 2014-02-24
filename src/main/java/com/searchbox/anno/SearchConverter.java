5591573dd43d223a595c33eeec5217971280b112
Stephane Gamard <stephane.gamard@searchbox.com>
2014-02-24 22:42:36 +0100

    Cleaned up the Search annotation

--- src/main/java/com/searchbox/anno/SearchConverter.java
+++ src/main/java/com/searchbox/anno/SearchConverter.java
@@ -9,7 +9,8 @@ import java.lang.annotation.Target;
 @Target({ ElementType.TYPE })
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
-public @interface SearchComponent {
+public @interface SearchConverter {
 	
-	
+	String urlParam();
+
 }
