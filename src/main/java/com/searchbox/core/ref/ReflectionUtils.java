1184027a8dad39e9772524b1ccef4a0cf8c9fd42
Stephane Gamard <stephane.gamard@searchbox.com>
2014-03-05 02:58:51 +0100

    Cleaned Imports

--- src/main/java/com/searchbox/core/ref/ReflectionUtils.java
+++ src/main/java/com/searchbox/core/ref/ReflectionUtils.java
@@ -22,12 +22,10 @@ import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.BeanInstantiationException;
 import org.springframework.beans.BeanUtils;
 
 import com.searchbox.core.SearchAttribute;
 import com.searchbox.framework.domain.UnknownAttributeDefinition;
-import com.searchbox.framework.web.SearchController;
 
 public class ReflectionUtils {
 
