fa4c45727eb2e4370a2f30b92fa57cbbe2a36b56
Stephane Gamard <stephane.gamard@searchbox.com>
2014-03-07 19:36:58 +0100

    Added Highlight to results

--- src/main/java/com/searchbox/core/search/result/TemplatedHitListAdapter.java
+++ src/main/java/com/searchbox/core/search/result/TemplatedHitListAdapter.java
@@ -1,20 +1,60 @@
 package com.searchbox.core.search.result;
 
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 import org.apache.solr.client.solrj.SolrQuery;
 import org.apache.solr.client.solrj.response.QueryResponse;
 import org.apache.solr.common.SolrDocument;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.searchbox.core.SearchAdapter;
 import com.searchbox.core.SearchAdapter.Time;
 import com.searchbox.core.SearchAdapterMethod;
+import com.searchbox.core.dm.FieldAttribute;
+import com.searchbox.core.dm.FieldAttribute.USE;
+import com.searchbox.engine.solr.SolrSearchEngine;
 
 @SearchAdapter
 public class TemplatedHitListAdapter  {
+	
+	@SuppressWarnings("unused")
+	private static final Logger LOGGER = LoggerFactory.getLogger(TemplatedHitListAdapter.class);
+	
+	@SearchAdapterMethod(execute=Time.PRE)
+	public void setHighlightFieldsForTemplate(SolrSearchEngine engine, TemplatedHitList searchElement,
+			SolrQuery query, FieldAttribute attribute) {
+		
+		if(!attribute.getHighlight()){
+			return;
+		}
+		
+		String fieldHighlightKey = engine.getKeyForField(attribute, USE.SEARCH);
+		
+		if(query.getHighlightFields() == null || 
+				!Arrays.asList(query.getHighlightFields()).contains(fieldHighlightKey)){
+			query.addHighlightField(fieldHighlightKey);			
+		}
+	}
 
+	@SearchAdapterMethod(execute=Time.PRE)
+	public void addHighlightFieldsForTemplate(TemplatedHitList searchElement,
+			SolrQuery query, FieldAttribute attribute) {
+		
+		if(!attribute.getHighlight()){
+			return;
+		}
+		if(query.getFacetFields() == null ||
+			!Arrays.asList(query.getFacetFields()).contains(attribute.getField().getKey())){
+			query.addField(attribute.getField().getKey());
+		}
+	}
+	
 	@SearchAdapterMethod(execute=Time.PRE)
 	public void setRequieredFieldsForTemplate(TemplatedHitList searchElement,
 			SolrQuery query) {
@@ -33,10 +73,19 @@ public class TemplatedHitListAdapter  {
 		}
 		fields.add("score");
 		query.setFields(fields.toArray(new String[0]));
+		
+		//Setting highlights parameters
+		query.setHighlight(true);
+		query.setHighlightSnippets(3);
 	}
 
 	@SearchAdapterMethod(execute=Time.POST)
-	public void generateHitElementsForTemplate(TemplatedHitList element, QueryResponse response) {
+	public void generateHitElementsForTemplate(TemplatedHitList element, QueryResponse response,
+			FieldAttribute attribute) {
+
+		if(!attribute.getId()){
+			return;
+		}
 		
 		Iterator<SolrDocument> documents = response.getResults().iterator();
 		while(documents.hasNext()){
@@ -45,7 +94,17 @@ public class TemplatedHitListAdapter  {
 			for(String field:document.getFieldNames()){
 				hit.addFieldValue(field, document.get(field));
 			}
+			//Now we push the highlights
+			Object id = document.getFirstValue(attribute.getField().getKey());
+			Map<String, List<String>> highlights = response.getHighlighting().get(id);
+			for(String highlihgtkey:highlights.keySet()){
+				for(String fieldkey:document.getFieldNames()){
+					if(highlihgtkey.contains(fieldkey)){
+						hit.getHighlights().put(fieldkey, highlights.get(highlihgtkey));
+						break;
+					}
+				}
+			}
 		}
 	}
-
 }
