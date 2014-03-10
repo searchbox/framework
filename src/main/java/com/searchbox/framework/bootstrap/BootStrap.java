62b6bb192ad1be1c3f3d0fa806d782b170a6bab9
Stephane Gamard <stephane.gamard@searchbox.com>
2014-03-10 23:06:57 +0100

    Fixed Bootstrap for idFieldName in Collection

--- src/main/java/com/searchbox/framework/bootstrap/BootStrap.java
+++ src/main/java/com/searchbox/framework/bootstrap/BootStrap.java
@@ -128,6 +128,7 @@ public class BootStrap implements ApplicationListener<ContextRefreshedEvent> {
 		/** The base collection for searchbox */
 		LOGGER.info("++ Creating pubmed Collection");
 		CollectionDefinition collection = new CollectionDefinition(PubmedCollection.class,"pubmed");
+		collection.setIdFieldName("id");
 		collection.setAutoStart(true);
 		collection.setSearchEngine(engine);	
 		collection = collectionRepository.save(collection);
@@ -138,9 +139,6 @@ public class BootStrap implements ApplicationListener<ContextRefreshedEvent> {
 		preset.setLabel("Search All");
 		preset.setSlug("all");
 		
-		FieldAttributeDefinition idFieldAttr = new FieldAttributeDefinition(collection.getFieldDefinition("id"));
-		idFieldAttr.setAttributeValue("id",true);
-		preset.addFieldAttribute(idFieldAttr);
 		
 		FieldAttributeDefinition fieldAttr = new FieldAttributeDefinition(collection.getFieldDefinition("article-title"));
 		fieldAttr.setAttributeValue("searchable",true);
