592cf211ee6b3846a262d1ceefd8a9bd194f14a6
Stephane Gamard <stephane.gamard@searchbox.com>
2014-03-13 18:38:00 +0100

    Added Collection Interfaces #ToUpStream

---
+++ src/main/java/com/searchbox/collection/ExpiringDocuments.java
@@ -0,0 +1,13 @@
+package com.searchbox.collection;
+
+import java.util.Date;
+
+import com.searchbox.core.dm.Collection.FieldMap;
+
+public interface ExpiringDocuments {
+   
+    public static final String STD_DEADLINE_FIELD = "sb_deadline";
+    
+    Date getDeadlineValue(FieldMap fields);
+
+}
