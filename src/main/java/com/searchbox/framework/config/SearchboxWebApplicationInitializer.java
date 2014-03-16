8d09f1012b1dbc26bb0f2be60755d851dc9b9876
Stephane Gamard <stephane.gamard@searchbox.com>
2014-03-16 09:46:13 +0100

    FRAM-6: fixed when container is compatible with UTF-8 URL params (jetty is by default)

--- src/main/java/com/searchbox/framework/config/SearchboxWebApplicationInitializer.java
+++ src/main/java/com/searchbox/framework/config/SearchboxWebApplicationInitializer.java
@@ -20,66 +20,66 @@ import javax.servlet.Filter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.orm.jpa.support.OpenEntityManagerInViewFilter;
+import org.springframework.web.filter.CharacterEncodingFilter;
 import org.springframework.web.filter.HiddenHttpMethodFilter;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
-public class SearchboxWebApplicationInitializer extends
-        AbstractAnnotationConfigDispatcherServletInitializer {
+public class SearchboxWebApplicationInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
 
-    private static final Logger LOGGER = LoggerFactory
-            .getLogger(SearchboxWebApplicationInitializer.class);
+  private static final Logger LOGGER = LoggerFactory.getLogger(SearchboxWebApplicationInitializer.class);
 
-    private static double JAVA_VERSION = getVersion();
+  private static double JAVA_VERSION = getVersion();
 
-    public SearchboxWebApplicationInitializer() {
-        super();
+  public SearchboxWebApplicationInitializer() {
+    super();
 
-        if (JAVA_VERSION < 1.7) {
-            LOGGER.error("Java 7 is required to run Searchbox. Current version: "
-                    + JAVA_VERSION + ". Please update your system.");
-            // throw new Exception("bad value");
-            System.exit(0);
-        }
+    if (JAVA_VERSION < 1.7) {
+      LOGGER.error("Java 7 is required to run Searchbox. Current version: " + JAVA_VERSION
+          + ". Please update your system.");
+      // throw new Exception("bad value");
+      System.exit(0);
     }
+  }
 
-    @Override
-    protected Class<?>[] getRootConfigClasses() {
-        return new Class[] { RootConfiguration.class };
-    }
+  @Override
+  protected Class<?>[] getRootConfigClasses() {
+    return new Class[] { RootConfiguration.class };
+  }
 
-    @Override
-    protected Class<?>[] getServletConfigClasses() {
-        return new Class[] { WebConfig.class };
-    }
+  @Override
+  protected Class<?>[] getServletConfigClasses() {
+    return new Class[] { WebConfig.class };
+  }
 
-    @Override
-    protected String[] getServletMappings() {
-        return new String[] { "/" };
-    }
+  @Override
+  protected String[] getServletMappings() {
+    return new String[] { "/" };
+  }
 
-    @Override
-    protected Filter[] getServletFilters() {
-        return new Filter[] { new HiddenHttpMethodFilter(),
-                new OpenEntityManagerInViewFilter() };
-    }
+  @Override
+  protected Filter[] getServletFilters() {
+    
+    return new Filter[] { new HiddenHttpMethodFilter(),
+        new OpenEntityManagerInViewFilter() };
+  }
 
-    /**
-     * Retrieving current java version
-     * 
-     * @return Double Java version
-     */
-    private static double getVersion() {
-        String version = System.getProperty("java.version");
-        LOGGER.info("Checking java version. Version found :" + version);
-        int pos = 0, count = 0;
-        for (; pos < version.length() && count < 2; pos++) {
-            if (version.charAt(pos) == '.') {
-                count++;
-            }
-        }
-        --pos; // EVALUATE double
-        double dversion = Double.parseDouble(version.substring(0, pos));
-        return dversion;
+  /**
+   * Retrieving current java version
+   * 
+   * @return Double Java version
+   */
+  private static double getVersion() {
+    String version = System.getProperty("java.version");
+    LOGGER.info("Checking java version. Version found :" + version);
+    int pos = 0, count = 0;
+    for (; pos < version.length() && count < 2; pos++) {
+      if (version.charAt(pos) == '.') {
+        count++;
+      }
     }
+    --pos; // EVALUATE double
+    double dversion = Double.parseDouble(version.substring(0, pos));
+    return dversion;
+  }
 
 }
