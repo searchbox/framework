44aef8b8877975e720c6bb12ecb80d15cb17ceca
Jonathan Rey <jonatrey@reziew.com>
2014-03-19 11:54:14 +0100

    Override get method on fieldmap to not return a null.

--- src/main/java/com/searchbox/core/dm/Collection.java
+++ src/main/java/com/searchbox/core/dm/Collection.java
@@ -16,6 +16,7 @@
 package com.searchbox.core.dm;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 
@@ -105,6 +106,20 @@ public class Collection {
   }
 
   public static class FieldMap extends HashMap<String, List<Object>> {
+	  
+	  /**
+	   * Returns an empty list if key doesn't exist
+	   * @param key String key
+	   * @return List<Object>
+	   */
+	  public List<Object> get(String key){
+		  if(super.containsKey(key)){
+			  return super.get(key);
+		  } else {
+			  return Collections.emptyList();
+		  }
+	  }
+	  
     /**
      * 
      */
