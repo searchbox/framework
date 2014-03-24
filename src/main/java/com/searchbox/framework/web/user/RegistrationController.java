dc736ca2b9b62385c05be1b942ea1f6935814d4b
Stephane Gamard <stephane.gamard@searchbox.com>
2014-03-24 10:30:19 +0100

    ResgistrationController was not accessible

--- src/main/java/com/searchbox/framework/web/user/RegistrationController.java
+++ src/main/java/com/searchbox/framework/web/user/RegistrationController.java
@@ -26,13 +26,14 @@ import com.searchbox.framework.util.SecurityUtil;
 
 @Controller
 @SessionAttributes("user")
+@RequestMapping("/user/**")
 public class RegistrationController {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(RegistrationController.class);
 
     protected static final String ERROR_CODE_EMAIL_EXIST = "NotExist.user.email";
     protected static final String MODEL_NAME_REGISTRATION_DTO = "user";
-    protected static final String VIEW_NAME_REGISTRATION_PAGE = "user/registrationForm";
+    protected static final String VIEW_NAME_REGISTRATION_PAGE = "user/register";
 
     private UserService service;
 
@@ -44,7 +45,7 @@ public class RegistrationController {
     /**
      * Renders the registration page.
      */
-    @RequestMapping(value = "/user/register", method = RequestMethod.GET)
+    @RequestMapping(value = "/register", method = RequestMethod.GET)
     public String showRegistrationForm(WebRequest request, Model model) {
         LOGGER.debug("Rendering registration page.");
 
@@ -84,7 +85,7 @@ public class RegistrationController {
     /**
      * Processes the form submissions of the registration form.
      */
-    @RequestMapping(value ="/user/register", method = RequestMethod.POST)
+    @RequestMapping(value ="/register", method = RequestMethod.POST)
     public String registerUserAccount(@Valid @ModelAttribute("user") RegistrationForm userAccountData,
                                       BindingResult result,
                                       WebRequest request) throws DataException {
